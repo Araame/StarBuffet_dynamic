7a4b129675ad006f0afe6303b1688337ccae08d1
Araame <arame171002@gmail.com>
2026-03-30 12:23:34 +0000

    Affichage done: etape 2

--- services/views.py
+++ services/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Traiteur
 # Create your views here.
+def liste_traiteurs(request):
+    """Retrieve all traiteurs"""
+    context = {}
+    context["traiteurs"] = Traiteur.objects.all()
+
+    return render(request,"liste.html", context = context)
