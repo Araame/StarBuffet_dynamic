cd6ad4de87096f6ccbaf4a8c01c6cb18fb922a93
Araame <arame171002@gmail.com>
2026-04-04 11:36:02 +0000

    Stylisation done

---
+++ services/admin.py
@@ -0,0 +1,5 @@
+from .models import Traiteur
+from django.contrib import admin
+
+
+admin.site.register(Traiteur)
--- starbuffet_project/urls.py
+++ starbuffet_project/urls.py
@@ -24,7 +24,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('traiteurs/', include("services.urls")),
+    path('', include("services.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
